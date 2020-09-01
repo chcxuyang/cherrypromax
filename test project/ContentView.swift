10813413c0bd80748c1a06271023ddae546fcce9
Yang Xu <chcxuyang@gmail.com>
2020-09-01 15:24:40 +1000

    this is a test message

---
+++ test project/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  test project
+//
+//  Created by Yang Xu on 1/9/20.
+//  Copyright © 2020 Yang Xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// this line is for testing
